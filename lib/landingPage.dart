c5c263f1ff34032e441f0073577fb531b531e705
unknown <bala@micropyramid.com>
2022-09-09 15:23:16 +0530

    implement screens

---
+++ lib/landingPage.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:interactive_tamil_parayana/const.dart';
+import 'package:interactive_tamil_parayana/utils.dart';
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({super.key});
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: screenHeight,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              colors: [Colors.blue, Colors.purple],
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+              stops: [0.0, 1.0],
+              tileMode: TileMode.clamp),
+        ),
+        alignment: Alignment.center,
+        child: Column(
+          children: [
+            SizedBox(height: screenHeight * 0.1),
+            Container(
+              height: screenHeight * 0.3,
+              width: screenWidth / 1.5,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/bhagavan2_500.9.png'),
+                    fit: BoxFit.fill),
+              ),
+            ),
+            SizedBox(height: screenHeight / 40),
+            Text('Parayana At Sri Ramanasramam',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: screenWidth / 20,
+                    fontWeight: FontWeight.bold)),
+            SizedBox(height: screenHeight / 40),
+            Text('The Poetic Works of \n Bhagavan Sri Ramana Maharshi',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Color.fromARGB(255, 0, 212, 64),
+                    fontSize: screenWidth / 20,
+                    fontWeight: FontWeight.bold)),
+            SizedBox(height: screenHeight / 40),
+            GestureDetector(
+                onTap: () async {
+                  Navigator.pushNamed(context, '/daysPage');
+                },
+                child: Container(
+                    margin: const EdgeInsets.all(20),
+                    padding: const EdgeInsets.all(10.0),
+                    decoration: const BoxDecoration(
+                      color: Colors.purpleAccent,
+                      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Image.asset(
+                          "assets/images/bhagavanround2.png",
+                          width: screenWidth * 0.1,
+                        ),
+                        SizedBox(
+                          width: screenWidth * 0.7,
+                          child: Text(
+                            "Tamil Parayana",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: screenWidth / 20,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
+                    ))),
+            //     GestureDetector(
+            // onTap: () async {},
+            // child: Container(
+            //     margin: const EdgeInsets.all(20),
+            //     padding: const EdgeInsets.all(10.0),
+            //     decoration: BoxDecoration(
+            //       color: Colors.purpleAccent,
+            //       border: Border.all(width: 1.0, color: Colors.grey),
+            //       borderRadius:
+            //           const BorderRadius.all(Radius.circular(5.0)),
+            //     ),
+            //     child: Row(
+            //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //       children: [
+            //         Image.asset(
+            //           "assets/images/bhagavanround2.png",
+            //           width: screenWidth * 0.1,
+            //         ),
+            //         SizedBox(
+            //           width: screenWidth * 0.7,
+            //           child: Text(
+            //             "Tamil Parayana",
+            //             style: TextStyle(
+            //                 color: Colors.white,
+            //                 fontSize: screenWidth / 20,
+            //                 fontWeight: FontWeight.bold),
+            //           ),
+            //         ),
+            //       ],
+            //     ))),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+
+TargetPlatform? platform;
+
+double screenHeight=0.0;
+double screenWidth=0.0;
+String selectedLanguage="Tamil";
+int currentDay=1;
+int currentSong = 1;
